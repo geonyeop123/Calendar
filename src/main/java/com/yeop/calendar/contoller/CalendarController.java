35775f3eaf3890219e831a838c37442c4abc1467
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-05-03 21:03:25 +0900

    Initial commit

---
+++ src/main/java/com/yeop/calendar/contoller/CalendarController.java
@@ -0,0 +1,15 @@
+package com.yeop.calendar.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CalendarController {
+
+    @RequestMapping("/")
+    public String home(){
+        asd
+        return "index";
+    }
+
+}
---
+++ src/test/java/com/yeop/calendar/CalendarApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yeop.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
